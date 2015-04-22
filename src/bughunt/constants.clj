8fc8702ccf031c14b212be6ca3e4545581d26470
Ruslan Kamaldinov <r.kamaldinov@gmail.com>
2015-04-22 21:57:56 +0300

    Re-formatted constants.clj

--- src/bughunt/constants.clj
+++ src/bughunt/constants.clj
@@ -1,15 +1,27 @@
 (ns bughunt.constants)
 
 (def BUG_STATUSES
-  ["New", "Incomplete", "Invalid",
-   "Won't Fix", "Confirmed", "Triaged",
-   "In Progress", "Fix Committed",
-   "Fix Released",
-   "Opinion", "Expired"])
+  ["Confirmed"
+   "Expired"
+   "Fix Committed"
+   "Fix Released"
+   "In Progress"
+   "Incomplete"
+   "Invalid"
+   "New"
+   "Opinion"
+   "Triaged"
+   "Won't Fix"])
 
-;; date_left_closed - when task was last reopened.
-;; date_left_new - when task was marked with a status higher than New.
 (def BUG_DATE_FIELDS
-  [:date_assigned :date_closed :date_confirmed :date_created
-   :date_fix_committed :date_fix_released :date_in_progress
-   :date_incomplete :date_left_closed :date_left_new :date_triaged])
+  [:date_assigned
+   :date_closed
+   :date_confirmed
+   :date_created
+   :date_fix_committed
+   :date_fix_released
+   :date_in_progress
+   :date_incomplete
+   :date_left_closed     ; date reopened
+   :date_left_new        ; date marked with a status higher than New
+   :date_triaged])
